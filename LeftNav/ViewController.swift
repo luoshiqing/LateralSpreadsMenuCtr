2251e3969045ccac594549e5bfd624392900c7e9
luoshiqing <644402920@qq.com>
2017-03-07 16:51:10 +0800

    Initial Commit

---
+++ LeftNav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeftNav
+//
+//  Created by sqluo on 2017/3/7.
+//  Copyright © 2017年 sqluo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
